51da24bf3b7f9b537f3d4e04b2c35f50d3b97610
Jonathan See <mannysee@gmail.com>
2017-06-01 09:21:41 +1000

    working short code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 
 var playerTurn = "X";
 
-var draw = 0;
-
 var numberOfTurns = 0;
 
 var scoreX = 0;
 var scoreO = 0;
 var scoreDraw = 0;
-
 
 var gameOver = false; // to initialise the game (because it's not yet over)
 
@@ -29,6 +26,25 @@ $(document).ready( function () {
     $('#winner').show();
   }
 
+  var checkWinState = function ( firstCell, secondCell, thirdCell, winSelect ) {
+    if (firstCell.text().length && firstCell.text() === secondCell.text() && secondCell.text() === thirdCell.text()) {
+      $(winSelect).css('backgroundColor', '#D9BBF9');
+      gameOver = true;
+      if (firstCell.text() === "X") {
+        showWin("X");
+        scoreX += 1 ;    // scoreX++;
+        $('#scoreX').text(scoreX);
+      } else {
+        showWin("O");
+        scoreO += 1;
+        $('#scoreO').text(scoreO);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   $('td').click( function () {
 
     if ( $( this ).text().length || gameOver ) { // If: (1) there is something ("X" or "O") in the box, prevents it from being clicked again; OR (2) game is over, prevents players from clicking or continuing with game.
@@ -37,139 +53,48 @@ $(document).ready( function () {
 
     $( this ).html( playerTurn );
     numberOfTurns += 1;
-    if ( playerTurn === "X" ) {
+    if ( playerTurn === "X" ) { // if statement to alternate X and O
       playerTurn = "O";
     } else { // playerTurn !== "X"
       playerTurn = "X";
     }
     //debugger;
-    if ($cellOne.text().length && $cellOne.text() === $cellTwo.text() && $cellTwo.text() === $cellThree.text()) {
-      // console.log("Player wins!");
-      $('.row.first td').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellOne.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    if ( checkWinState( $cellOne, $cellTwo, $cellThree, '.row.first td' ) ) {
       return;
-    } else if ($cellFour.text().length && $cellFour.text() === $cellFive.text() && $cellFive.text() === $cellSix.text()) {
-      // console.log("Player wins!");
-      $('.row.second td').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellFour.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
-      return;
-    } else if ($cellSeven.text().length && $cellSeven.text() === $cellEight.text() && $cellEight.text() === $cellNine.text()) {
-      // console.log("Player wins!");
-      $('.row.third td').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellSeven.text() === "X") {
-      showWin("X");
-      scoreX += 1 ;    // scoreX++;
-      $('#scoreX').text(scoreX);
-    } else {
-      showWin("O");
-      scoreO += 1;
-      $('#scoreO').text(scoreO);
     }
+    if ( checkWinState( $cellFour, $cellFive, $cellSix, '.row.second td' ) ) {
       return;
-    } else if ($cellOne.text().length && $cellOne.text() === $cellFour.text() && $cellFour.text() === $cellSeven.text()) {
-      // console.log("Player wins!");
-      $('tr td:first-child').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellOne.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    }
+    if ( checkWinState( $cellSeven, $cellEight, $cellNine, '.row.third td' ) ) {
       return;
-    } else if ($cellTwo.text().length && $cellTwo.text() === $cellFive.text() && $cellFive.text() === $cellEight.text()) {
-      // console.log("Player wins!");
-      $('tr td:nth-child(2)').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellTwo.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    }
+    if ( checkWinState( $cellOne, $cellFour, $cellSeven, 'tr td:first-child' ) ) {
       return;
-    } else if ($cellThree.text().length && $cellThree.text() === $cellSix.text() && $cellSix.text() === $cellNine.text()) {
-      // console.log("Player wins!");
-      $('tr td:nth-child(3)').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellThree.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    }
+    if ( checkWinState( $cellTwo, $cellFive, $cellEight, 'tr td:nth-child(2)' ) ) {
       return;
-    } else if ($cellOne.text().length && $cellOne.text() === $cellFive.text() && $cellFive.text() === $cellNine.text()) {
-      // console.log("Player wins!");
-      $('.diag1').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellOne.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    }
+    if ( checkWinState( $cellThree, $cellSix, $cellNine, 'tr td:nth-child(3)' ) ) {
       return;
-    } else if ($cellThree.text().length && $cellThree.text() === $cellFive.text() && $cellFive.text() === $cellSeven.text()) {
-      // console.log("Player wins!");
-      $('.diag2').css('backgroundColor', '#D9BBF9');
-      gameOver = true;
-      if ($cellThree.text() === "X") {
-        showWin("X");
-        scoreX += 1 ;    // scoreX++;
-        $('#scoreX').text(scoreX);
-      } else {
-        showWin("O");
-        scoreO += 1;
-        $('#scoreO').text(scoreO);
-      }
+    }
+    if ( checkWinState( $cellOne, $cellFive, $cellNine, '.diag1' ) ) {
       return;
-
-    } else if( numberOfTurns === 9) {
+    }
+    if ( checkWinState( $cellThree, $cellFive, $cellSeven, '.diag2' ) ) {
+      return;
+    }
+    else if ( numberOfTurns === 9) {
       // no winning combo found, so check for draw
 
       /* all boxes are occupied - how to check?
          1. check length of each cell contents
          2. check how many turns have been played
-
       */
       scoreDraw += 1;
       $('#scoreDraw').text(scoreDraw);
       $('#draw').show();
 
     }
-
 
   });
 
